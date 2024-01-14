e150879e37125a195d49e168e4549ee7d893feda
Salvador Norberto Vega Luna <94280842+senddalton@users.noreply.github.com>
2024-01-14 13:15:40 -0600

    se agrega el proyecto

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once("c://xampp/htdocs/xd/view/head/header.php")
+?>
+
+
+<?php
+require_once("c://xampp/htdocs/xd/view/head/fooder.php")
+?>
---
+++ view/head/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+     <link rel="stylesheet" href="/xd/aset/css/estilo.css">
+     <script src="https://kit.fontawesome.com/690af88597.js" crossorigin="anonymous"></script>
+  
+    </head>
+  <body>
+    
+  
